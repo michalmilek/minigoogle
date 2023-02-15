a5ef1777249b3ff41e1225f69ac4c4f9c6fb031e
michalmilek <mmilek1337@gmail.com>
2023-02-15 14:29:01 +0100

    v20

---
+++ .history/pages/api/auth/[...nextauth]_20230215141434.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+        // @ts-ignore
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      // @ts-ignore
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.NEXT_PUBLIC_SECRET_KEY,
+      pages: {
+        signIn: "/auth/signin"
+    }
+}
+export default NextAuth(authOptions)
